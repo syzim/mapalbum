f39de67eafe16a7ace1b66169e48d521eedc3933
Kieran Sisam <kieransisam@gmail.com>
2017-07-06 10:37:47 +1200

    fixed bug where a region with an extra div would try and create when only one photo in region

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var favicon = require('serve-favicon');
 
-app.use(express.static('dist'));
+app.use(express.static('src'));
 
 
-app.use(favicon('src/favicon2.png'));
+app.use(favicon('src/favicon.png'));
 app.listen(3000,function(){
   console.log("USA Tour Listening on Port 3000!")
 })
